6e41c7b0216c9aecdc5965c2631ba54eb37a477e
Theoluky <theoluk@gmail.com>
2025-05-15 13:36:00 -0600

    Fix json parsing

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ AUTHORIZED_NUMBERS = os.environ.get("AUTHORIZED_NUMBER")  # replace this
 
 @app.route("/sms", methods=["POST"])
 def receive_sms():
-    data = request.json
+    fulljson = request.json
+    data = fulljson.get("data")
     from_number = data.get("from")
     body = data.get("text", "").strip()
     print(data)
@@ -24,7 +25,7 @@ def receive_sms():
 
     reply = handle_message(body)
     # send_sms(from_number, reply)
-    print("reply")
+    print(from_number, reply)
     return Response(status=200)
 
 def handle_message(text):
@@ -45,3 +46,7 @@ def send_sms(to, message):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+# Example data from Telnyx:
+# {'data': {'event_type': 'message.received', 'id': '5ed9ec04-a33f-4ef2-b452-3a2989fac327', 'occurred_at': '2025-05-15T18:47:41.038+00:00', 'payload': {'autoresponse_type': None, 'cc': [], 'completed_at': None, 'cost': {'amount': '0.0160', 'currency': 'USD'}, 'cost_breakdown': {'carrier_fee': {'amount': '0.01200', 'currency': 'USD'}, 'rate': {'amount': '0.00400', 'currency': 'USD'}}, 'direction': 'inbound', 'encoding': 'GSM-7', 'errors': [], 'from': {'carrier': 'T-Mobile USA', 'line_type': 'Wireless', 'phone_number': '+18578698297'}, 'id': '1fa55452-5cc6-4ce7-9cb0-8612fa4ddc15', 'is_spam': False, 'media': [], 'messaging_profile_id': '400196d4-e32b-42c2-9654-190ffd5d9fb7', 'organization_id': 'ada11ebf-7c78-4f9d-914b-001da12f72d2', 'parts': 1, 'received_at': '2025-05-15T18:47:40.731+00:00', 'record_type': 'message', 'sent_at': None, 'subject': '', 'tags': [], 'text': 'Test for full JSON', 'to': [{'carrier': 'Telnyx', 'line_type': 'Wireless', 'phone_number': '+17179379708', 'status': 'webhook_delivered'}], 'type': 'SMS', 'valid_until': None, 'webhook_failover_url': None, 'webhook_url': 'https://sms-assistant-90xc.onrender.com/sms'}, 'record_type': 'event'}, 'meta': {'attempt': 1, 'delivered_to': 'https://sms-assistant-90xc.onrender.com/sms'}}
