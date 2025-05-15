4af82d8b87917f49de1eb4b5331c6b63f1bfbabf
Theoluky <theoluk@gmail.com>
2025-05-15 11:52:54 -0600

    Add new files

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+from flask import Flask, request, Response
 import requests
+import os
+
+app = Flask(__name__)
+
+TELNYX_API_KEY = os.environ.get("TELNYX_API_KEY")
+TELNYX_MESSAGING_PROFILE_ID = os.environ.get("TELNYX_MESSAGING_PROFILE_ID")
+AUTHORIZED_NUMBERS = {"+18578698297"}  # replace this
 
